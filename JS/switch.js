fffa78bd83bcc7b702799a7efff9f4b6d17102d4
rcortezk9 <rcortezk9@gmail.com>
2017-04-10 15:56:38 -0500

    -made changes to the structure, put the console log at the end -added new var

--- JS/switch.js
+++ JS/switch.js
@@ -5,30 +5,31 @@
 
 var luckyNumber = Math.floor(Math.random()* 6)
 var cost = 60;
+var total;
+
 
 switch (luckyNumber) {
-    case 0:
-        console.log("You have no discount, your cost is $" + cost);
-        break;
     case 1:
-        cost = cost - (cost * .10)
-        console.log("You get a 10% discount, your cost is $" + cost);
+        total = cost - (cost * .10);
+        console.log("You get a 10% discount");
         break;
     case 2:
-        cost = cost - (cost * .25)
-        console.log("You get a 25% discount, you pay $" + cost);
+        total = cost - (cost * .25);
+        console.log("You get a 25% discount");
         break;
     case 4:
-        cost = cost - (cost * .50)
-        console.log("You get a 50% discount, you pay $" + cost);
+        total = cost - (cost * .50);
+        console.log("You get a 50% discount");
         break;
     case 5:
-        cost = cost - (cost * 1)
-        console.log("You get it for free, you pay $" + cost);
+        total = 0;
+        console.log("You get it for free");
         break;
     default:
-        console.log("I don't know what to tell you.")
+        total = cost;
+        console.log("Sorry no discount for you.")
 }
+    console.log("Your total is $" + total.toFixed(2));
 
 var numMonth = Math.floor(Math.random()* 14);
 

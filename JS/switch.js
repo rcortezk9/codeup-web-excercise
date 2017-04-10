9edb44719d59a117af84b165e83a9f10b2afa8b1
rcortezk9 <rcortezk9@gmail.com>
2017-04-10 15:31:53 -0500

    -made changes to the output text.

--- JS/switch.js
+++ JS/switch.js
@@ -30,45 +30,45 @@ switch (luckyNumber) {
         console.log("I don't know what to tell you.")
 }
 
-var numMonth = Math.floor(Math.random()* 13);
+var numMonth = Math.floor(Math.random()* 14);
 
 switch (numMonth) {
     case 1:
-        console.log("January");
+        console.log("Your month is January.");
         break;
     case 2:
-        console.log("Feburary");
+        console.log("Your month is Feburary.");
         break;
     case 3:
-        console.log("March");
+        console.log("Your month is March.");
         break;
     case 4:
-        console.log("April");
+        console.log("Your month is April.");
         break;
     case 5:
-        console.log("May");
+        console.log("Your month is May.");
         break;
     case 6:
-        console.log("June");
+        console.log("Your month is June.");
         break;
     case 7:
-        console.log("July");
+        console.log("Your month is July.");
         break;
     case 8:
-        console.log("August");
+        console.log("Your month is August.");
         break;
     case 9:
-        console.log("September");
+        console.log("Your month is September.");
         break;
     case 10:
-        console.log("October");
+        console.log("Your month is October.");
         break;
     case 11:
-        console.log("November");
+        console.log("Your month is November.");
         break;
     case 12:
-        console.log("December");
+        console.log("Your month is December.");
         break;
     default:
-        console.log("Please try again.")
+        console.log("No such month.")
 }

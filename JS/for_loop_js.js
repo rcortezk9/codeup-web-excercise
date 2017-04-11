82deccb0b25d6b344ba56d5a12e35775d9060c55
rcortezk9 <rcortezk9@gmail.com>
2017-04-11 15:37:27 -0500

    -made corrections to syntax errors on js file

--- JS/for_loop_js.js
+++ JS/for_loop_js.js
@@ -1,26 +1,42 @@
 /**
  * Created by renecortez on 4/11/17.
  */
+"use strict";
 
+// =========EXERCISE 1========= //
 
-var number = Math.floor(Math.random() * 10) + 1;
-
-
-for (i = 1; i <= 10; ++i) {
-    var total = number * i;
-    console.log(number + " x " + i + " = " + total);
+console.log(" ");
+console.log("Exercise 1...");
+var random = Math.floor((Math.random() * 10) + 1);
+for (var i = 1; i <= 10; ++i) {
+    var total = i * random;
+    console.log(random + "x" + i + "=" + total);
 }
 
+// =========EXERCISE 2========= //
+
+console.log(" ");
+console.log("Exercise 2...");
+
+
 for (var i = 1; i <= 10; ++i) {
-    var random = Math.floor(Math.random() (200 - 20 + 1)) + 20;
+    var random = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
     if (random % 2) {
-        console.log(random + "is odd.");
+        console.log(random + " is odd.");
     } else {
         console.log(random + " is even.");
     }
 }
 
 
+
+// =========EXERCISE 3========= //
+
+console.log(" ");
+console.log("Exercise 3...");
+
 for (var i = 100; i >= 5; i -= 5) {
     console.log(i);
 }
+
+

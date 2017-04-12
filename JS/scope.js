ae54bd85d99096c1f2072fdec62b5892151320ff
rcortezk9 <rcortezk9@gmail.com>
2017-04-12 11:33:38 -0500

    -refactor the code using IIFE so that there are no var and functions in the global scope

--- JS/scope.js
+++ JS/scope.js
@@ -2,50 +2,55 @@
  * Created by renecortez on 4/12/17.
  */
 "use strict";
+(function () {
 
-var myNameIs = 'René' // TODO: Fill in your name here.
 
-/**
- * TODO:
- * Create a function called 'sayHello' that takes a parameter 'name'.
- * When called, the function should return a message that says hello to the passed in name.
- *
- * Example
- *  > sayHello("codeup") // returns "Hello, codeup!"
- */
-(function sayHello(name) {
-    console.log("Hello, " + name + "!");
-})
-/**
- * TODO:
- * Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
- * Store the result of the function call in a variable named 'helloMessage'.
- * console.log 'helloMessage' to check your work
- */
-(sayHello(myNameIs));
+    var myNameIs = 'René'; // TODO: Fill in your name here.
+
+    /**
+     * TODO:
+     * Create a function called 'sayHello' that takes a parameter 'name'.
+     * When called, the function should return a message that says hello to the passed in name.
+     *
+     * Example
+     *  > sayHello("codeup") // returns "Hello, codeup!"
+     */
+    function sayHello(name) {
+        console.log("Hello, " + name + "!");
+    }
+
+    /**
+     * TODO:
+     * Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+     * Store the result of the function call in a variable named 'helloMessage'.
+     * console.log 'helloMessage' to check your work
+     */
+    sayHello(myNameIs);
 
 // Don't modify the following line, it generates a random number between 1 and
 // 100 and stores it in a variable named random
-var random = Math.floor((Math.random()*100)+1);
+    var random = Math.floor((Math.random() * 100) + 1);
 
-console.log("Random number is: " + random.toString());
+    console.log("Random number is: " + random.toString());
 
-/**
- * TODO:
- * Create a function called 'isOdd' that takes a number as a parameter.
- * The function should use the ternary operator to return a message.
- * The message should contain the number being checked, and whether or not the
- * number is odd
- *
- * Example
- *  > isOdd(42) // returns "42 is not odd!"
- */
-(function isOdd(numberToTest) {
-    (numberToTest % 2 === 1) ? console.log("Number is odd.") : console.log("Number is even.");
-})
-/**
- * TODO:
- * Call the function 'isOdd' passing the variable 'random' as a parameter.
- * console.log *outside of the function* to check your work
- */
-(isOdd(random));
+    /**
+     * TODO:
+     * Create a function called 'isOdd' that takes a number as a parameter.
+     * The function should use the ternary operator to return a message.
+     * The message should contain the number being checked, and whether or not the
+     * number is odd
+     *
+     * Example
+     *  > isOdd(42) // returns "42 is not odd!"
+     */
+    function isOdd(numberToTest) {
+        (numberToTest % 2 === 1) ? console.log("Number is odd.") : console.log("Number is even.");
+    }
+
+    /**
+     * TODO:
+     * Call the function 'isOdd' passing the variable 'random' as a parameter.
+     * console.log *outside of the function* to check your work
+     */
+    isOdd(random);
+})();

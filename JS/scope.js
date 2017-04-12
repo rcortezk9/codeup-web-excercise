f9c1cb564d93989c00284802acada5d17b2cc16e
rcortezk9 <rcortezk9@gmail.com>
2017-04-12 11:27:25 -0500

    -refactor the code using IIFE so that there are no var and functions in the global scope

--- JS/scope.js
+++ JS/scope.js
@@ -1,3 +1,6 @@
+/**
+ * Created by renecortez on 4/12/17.
+ */
 "use strict";
 
 var myNameIs = 'René' // TODO: Fill in your name here.
@@ -10,16 +13,16 @@ var myNameIs = 'René' // TODO: Fill in your name here.
  * Example
  *  > sayHello("codeup") // returns "Hello, codeup!"
  */
-function sayHello(name) {
+(function sayHello(name) {
     console.log("Hello, " + name + "!");
-}
+})
 /**
  * TODO:
  * Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
  * Store the result of the function call in a variable named 'helloMessage'.
  * console.log 'helloMessage' to check your work
  */
-sayHello(myNameIs);
+(sayHello(myNameIs));
 
 // Don't modify the following line, it generates a random number between 1 and
 // 100 and stores it in a variable named random
@@ -37,12 +40,12 @@ console.log("Random number is: " + random.toString());
  * Example
  *  > isOdd(42) // returns "42 is not odd!"
  */
-function isOdd(numberToTest) {
+(function isOdd(numberToTest) {
     (numberToTest % 2 === 1) ? console.log("Number is odd.") : console.log("Number is even.");
-}
+})
 /**
  * TODO:
  * Call the function 'isOdd' passing the variable 'random' as a parameter.
  * console.log *outside of the function* to check your work
  */
-isOdd(random);
+(isOdd(random));

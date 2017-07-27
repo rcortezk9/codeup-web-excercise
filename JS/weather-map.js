82f06ca8d90edf84f75d73d973ce8a64707d96ed
rcortezk9 <rcortezk9@gmail.com>
2017-07-27 14:37:11 -0500

    added geocoder added eventlistener

--- JS/weather-map.js
+++ JS/weather-map.js
@@ -4,6 +4,7 @@
 $(document).ready(function(){
     'use strict';
     //declare variables
+    var search = document.getElementById("search");
     var lat = 29.4241;
     var lon = -98.4636;
     var mapOptions = {
@@ -14,6 +15,9 @@ $(document).ready(function(){
         },
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    var address, object;
+    var geocoder = new google.maps.Geocoder();
+
     // Render the map
     var map = new google.maps.Map(document.getElementById("map-display"), mapOptions);
     //draggable marker
@@ -27,6 +31,22 @@ $(document).ready(function(){
         animation: google.maps.Animation.DROP
     });
 
+    function locationGeoCode() {
+        // console.log(address.value + ' is the address you are searching');
+
+        geocoder.geocode({"address": address.value}, function (results, status) {
+            console.log(status);
+            console.log(results);
+            if (status === google.maps.GeocoderStatus.OK){
+                map.setCenter(results[0].geometry.location);
+
+            } else {
+                alert("Geocoding was not successful - STATUS: " + status)
+            }
+        });
+        address.value = '';
+    }
+
     // Function to convert output to title case
     function titleCase(str) {
         str = str.toLowerCase().split(' ');
@@ -35,13 +55,13 @@ $(document).ready(function(){
         }
         return str.join(' ');
     }
-
+    // Will give a compass direction
     function degToCompassDir(deg) {
         var compassDir = ["N","NNE","NE","ENE","E","ESE","SE","SSE","S","SSW","SW","WSW","W","WNW","NW","NNW","N"];;
         return compassDir [Math.round(deg/22.5)];
     }
 
-    function toggleColClass(days, cols) {
+    function toggleColClass(days, cols) {// Change format of display considering how many day forecast.
         for(var i=1; i<=days; i++) {
             if(days % 4 === 0) {
                 $('#day' + i).toggleClass('col-xs-6 col-sm-' + cols + ' hide');
@@ -55,25 +75,15 @@ $(document).ready(function(){
     function getDate(day) {  //today: day=0, tomorrow: day=1, dayAfterTomorrow: day=2 ...
 
         var today = new Date(), date, indexDayOfWeek;
-
         var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
-
         var month = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-
         if(day != 0) {
-
             day = new Date(today.getTime() + day * (24 * 60 * 60 * 1000));
-
             indexDayOfWeek = day.getDay();
-
             date = month[day.getMonth()] + ' ' + day.getDate();
-
             return '<span class="dayOfWeek">' + dayOfWeek[indexDayOfWeek] + '</span> <br>' + date;
-
         } else {
-
             return '<span class="dayOfWeek"> Today </span> <br>' + month[today.getMonth()] + ' ' + today.getDate();
-
         }
 
     }
@@ -86,11 +96,11 @@ $(document).ready(function(){
             units: "imperial",
             cnt: 10
         }).done(function (data) {
-            console.log(data);
+            // console.log(data);
             data.list.forEach(function (element, index) {
-//            console.log(element);
+           // console.log(element);
 
-                var content = "";
+                var content;
 
                 content = getDate(index);
                 content += '<br><span id="temp">' + parseInt(element.temp.max) + '&deg;' + '/' + parseInt(element.temp.min) + '&deg;</span>';
@@ -103,8 +113,10 @@ $(document).ready(function(){
                 $('#day' + (index+1)).html(content);
 
             });
-            var cityName = data.city.name;
-            $('#cityName').text(cityName);
+
+            $('#cityName').text(data.city.name);
+
+
         });
 
     $('.options').click(function () {
@@ -139,12 +151,12 @@ $(document).ready(function(){
         getWeather(lat, lon);
     });
 
-    $('#btn-go').click(function (event) {
-        lat = event.latLng.lat();
-        lon = event.latLng.lng();
-        mapOptions.center.lng = lon;
-        mapOptions.center.lat = lat;
-        getWeather(lat, lon);
+    search.addEventListener('click', function (e) {
+        e.preventDefault();
+        address = document.getElementById('location');
+        console.log(address.value + 'is the address you are looking for');
+        locationGeoCode();
+
     });
 
 
